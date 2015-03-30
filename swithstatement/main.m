16fe6da382ba4cbebb1b463cfdf1e0b448a8c0b9
Your Name <everettchurchey@crbc.edu>
2015-03-30 10:58:59 -0400

    Initial Commit

---
+++ swithstatement/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  swithstatement
+//
+//  Created by Ej Churchey on 3/30/15.
+//  Copyright (c) 2015 Ej Churchey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
